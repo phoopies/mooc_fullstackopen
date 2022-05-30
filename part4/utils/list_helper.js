6019351ff56f4d9e2b46e57d1288cab7edd10fa9
stefu <sotajagic@gmail.com>
2022-05-30 19:41:53 +0300

    part 4.7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require("lodash");
+
 const dummy = blogs => {
     return 1;
 };
@@ -6,8 +8,23 @@ const totalLikes = (blogs) => {
     return blogs.reduce((prev, blog) => prev + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((prev, blog) => blog.likes > (prev ? prev.likes : 0) ? blog : prev, undefined);
+};
+
+const mostBlogs = (blogs) => {
+    return lodash.chain(blogs).groupBy('author').map((val, key) => ({'author': key, 'blogs': val.length})).maxBy('blogs').value();
+};
+
+const mostLikes = (blogs) => {
+    return lodash.chain(blogs).groupBy('author').map((val, key) => ({'author': key, 'likes': lodash.reduce(val, (prev, blog) => prev + blog.likes, 0)})).maxBy('likes').value();
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 };
 
