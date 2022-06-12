8e9b2f43ccb44ef52bba4f2a0e228f712ccae5b3
phoopies <sotajagic@gmail.com>
2022-06-12 22:01:00 +0300

    part 6.19

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,17 +1,23 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 
-const Notification = () => {
-    const notification = useSelector((state) => state.notification);
-
+const Notification = (props) => {
     const style = {
         border: 'solid',
         padding: 10,
         borderWidth: 1,
     };
 
-    return notification !== '' ? (
-        <div style={style}>{notification}</div>
+    return props.notification !== '' ? (
+        <div style={style}>{props.notification}</div>
     ) : undefined;
 };
 
-export default Notification;
+const mapStateToProps = (state) => {
+    return {
+        notification: state.notification,
+    };
+};
+
+const ConnectedNotification = connect(mapStateToProps)(Notification);
+export default ConnectedNotification;
