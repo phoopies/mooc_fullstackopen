131a42ec3c6337c52b40fd469beefc31a8d9dec7
phoopies <sotajagic@gmail.com>
2022-07-05 13:30:22 +0300

    part 9.4

--- part9/a/index.ts
+++ part9/a/index.ts
@@ -1,5 +1,12 @@
-console.log(`
-HELP:
+import express from "express";
+const app = express();
 
-...
-`);
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
